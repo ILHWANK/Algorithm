c0b2d2d9fa4bd88f25b1ad6456b6c2a427ead1e1
WILLSON <dodjungvv@naver.com>
2019-07-09 02:53:34 +0900

    TestGame

--- problem/src/blackjack/BlackJackGsme.java
+++ problem/src/blackjack/BlackJackGsme.java
@@ -4,5 +4,6 @@ package blackjack;
 public class BlackJackGsme {
 	public static void main(String[] args) {
 		System.out.println("Make Start a BlackJack Game!");
+		System.out.println("Start Game");
 	}
 }
